80f942d73ce0d380a31bc48a2a981349600b5f29
Pawan <poojary.pawan16@gmail.com>
2021-01-01 02:40:25 +0530

    trial

--- freestyle/freestyleJS.js
+++ freestyle/freestyleJS.js
@@ -1,4 +1,4 @@
-console.log("connected?");
+// console.log("connected?");
 var canvas = document.querySelector("canvas");
 canvas.width =500;
 canvas.height=500;
@@ -30,3 +30,35 @@ c.stroke();
 c.lineTo(400,300);
 c.strokeStyle = "red";
 c.stroke();
+
+
+var x =0;
+var y = 0
+function animate(){
+//     console.log("wer r they");
+    c.clearRect(160,160,80,80);
+//     for(i=0; i<=100; i++){
+    c.beginPath();
+    c.arc(y,200, 30, 0, x,false);
+    c.strokeStyle = "red";
+    c.stroke();
+//     }
+
+
+
+if(x<=(Math.PI*2)&&y==200){
+    //     console.log("done");
+    x +=0.1;
+}else{
+    x -=0.1; 
+    c.strokeStyle = "green";
+    c.stroke();  
+}
+
+if(y<200){
+    y++;
+}
+requestAnimationFrame(animate);
+
+}
+animate();
