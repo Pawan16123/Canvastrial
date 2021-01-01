dfc162835ba0d03d82973899b6df3a6604c828d7
Pawan <poojary.pawan16@gmail.com>
2021-01-02 00:33:07 +0530

    muticolbal

--- freestyle/freestyleJS.js
+++ freestyle/freestyleJS.js
@@ -38,8 +38,13 @@ c.stroke();
 var circles = 0;
 var dy = 3;
 var dx = 3;
+var dxx= 2;
+var dx2 = 1;
 
 var ran = Math.floor(Math.random()*400 + 60);
+var ran1 = Math.floor(Math.random()*400 + 60);
+var ran2 = Math.floor(Math.random()*400 + 60);
+console.log(ran,ran1,ran2);
 // var rany = Math.floor(Math.random()*400);
 var rany = 200;
 
@@ -48,24 +53,38 @@ function animate(){
     var r =Math.floor(Math.random()*255);
     var g =Math.floor(Math.random()*255);
     var b =Math.floor(Math.random()*255);
-    c.clearRect(0,0,500,500);
+    // c.clearRect(0,0,500,500);
 
     c.beginPath();
     c.arc(ran,rany, 30, 0, 6.48,false);
+    c.stroke();
+    c.beginPath();
+    c.arc(ran1,rany, 30, 0, 6.48,false);
+    c.stroke();
+    c.beginPath();
+    c.arc(ran2,rany, 30, 0, 6.48,false);
     // c.strokeStyle = "#"+color1+color;
-    // c.strokeStyle = 'rgba('+r+','+ g+','+ b+','+ '1)';
-    c.strokeStyle ="magenta";
+    c.strokeStyle = 'rgba('+r+','+ g+','+ b+','+ '1)';
+    // c.strokeStyle ="magenta";
     c.stroke();
 
     if(rany>466 || rany<34){
         dy = -dy;
     }
+    if(ran1>466 || ran1<34){
+        dxx = -dxx;
+    }
+    if(ran2>466  || ran2<34){
+        dx2 = -dx2;
+    }
     if(ran>466 || ran<34){
         dx = -dx;
     }
-    requestAnimationFrame(animate);
     rany+= dy;
     ran+= dx;
+    ran1+= dxx;
+    ran2+= dx2;
+    requestAnimationFrame(animate);
     // if(circles<100-1){
 
     //     requestAnimationFrame(animate);
